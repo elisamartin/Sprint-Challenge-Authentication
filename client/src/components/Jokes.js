eac0cc3e7b6723f96de9dd0b9d1a98efde6cc0dd
elisamartin <elisamv93@gmail.com>
2019-04-05 11:34:13 +0200

    Components functionality

--- client/src/components/Jokes.js
+++ client/src/components/Jokes.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			jokes: []
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get('http://localhost:3300/api/jokes', {
+				headers: {
+					Authorization: localStorage.getItem('token')
+				}
+			})
+			.then((res) => {
+				this.setState({ jokes: res.data });
+			});
+	}
 	render() {
 		return (
 			<div>
 				<h2>Jokes</h2>
+				<ul>{this.state.jokes.map((joke) => <li key={joke.id}>{joke.joke}</li>)}</ul>
 			</div>
 		);
 	}
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			password: '',
+			department: ''
+		};
+	}
+
+	handleSubmit = () => {
+		axios
+			.post('http://localhost:3300/api/login', {
+				username: this.state.username,
+				password: this.state.password
+			})
+			.then((res) => {
+				localStorage.setItem('token', res.data.token);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	changeHandler = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
 	render() {
 		return (
 			<div className="login">
 				<h3>Login here</h3>
-				<form>
-					<p>username</p> <input type="text" name="username" />
-					<p>password</p> <input type="text" name="password" />
+				<form onSubmit={this.handleSubmit}>
+					<p>username</p> <input type="text" name="username" onChange={this.changeHandler} />
+					<p>password</p> <input type="text" name="password" onChange={this.changeHandler} />
 					<button type="submit">Submit</button>
 				</form>
 			</div>
