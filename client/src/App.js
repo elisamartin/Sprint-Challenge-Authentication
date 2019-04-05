e801ebc5740a85dccdf39cc039541a8587606cb5
elisamartin <elisamv93@gmail.com>
2019-04-05 11:01:51 +0200

    Stretch: cra starter

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">react</header>
+			</div>
+		);
+	}
+}
+
+export default App;
