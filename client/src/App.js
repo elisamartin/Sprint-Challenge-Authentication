daa28162c120a7034f51660ea2d29051b016f3bf
elisamartin <elisamv93@gmail.com>
2019-04-05 12:01:31 +0200

    Started styling

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,16 @@ class App extends Component {
 			<div className="App">
 				<h1>Dad Jokes React App</h1>
 				<nav>
-					<NavLink to="/">Login</NavLink>
-					<NavLink to="/jokes">Jokes</NavLink>
 					<NavLink to="/register">Register</NavLink>
+					<NavLink to="/login">Login</NavLink>
+					<NavLink to="/jokes">Jokes</NavLink>
 				</nav>
 				<div>
-					<Route exact path="/" component={Login} />
-					<Route exact path="/jokes" component={Jokes} />
 					<Route exact path="/register" component={Register} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/jokes" component={Jokes} />
 				</div>
+				<img alt="dadjoke" src="https://bit.ly/2G2qnBu" />
 			</div>
 		);
 	}
